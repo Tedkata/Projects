93e6561817fb2dd8f1d87ac765865f4d40e46d00
Tedkata <94693059+Tedkata@users.noreply.github.com>
2022-01-26 15:24:55 +0200

    Update MyDoublyLinkedList.py

--- MyDoublyLinkedList.py
+++ MyDoublyLinkedList.py
@@ -304,3 +304,58 @@ print(my_tree.contains(17))
 print(my_tree.min_value_node(my_tree.root))
 print(my_tree.min_value_node(my_tree.root.right))
 
+### My Hash Table
+
+class HashTable:
+    def __init__(self, size = 7):
+        self.data_map = [None] * size
+
+    def __hash(self, key):
+        my_hash = 0
+        for letter in key:
+            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+        return my_hash
+
+    def print_table(self):
+        for i, val in enumerate(self.data_map):
+            print(i, ": ", val)
+
+    def set(self, key, value):
+        index = self.__hash(key)
+        if self.data_map[index] == None:
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])
+
+    def get_item(self, key):
+        index = self.__hash(key)
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i][1]
+            return None
+
+    def keys(self):
+        all_keys = []
+        for i in range(len(self.data_map)):
+            if self.data_map[i] is not None:
+                for j in range(len(self.data_map[i])):
+                    all_keys.append(self.data_map[i][j][0])
+        return all_keys
+
+
+
+
+my_hash_table = HashTable()
+
+my_hash_table.set('bolts', 1400)
+my_hash_table.set('washers', 50)
+my_hash_table.set('lumber', 70)
+
+print(my_hash_table.keys())
+
+print(my_hash_table.get_item('bolts'))
+print(my_hash_table.get_item('washers'))
+print(my_hash_table.get_item('lumber'))
+
+
+
